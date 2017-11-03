82c6d377dc59f9c1d4f7c2be2855deb5dd15cda9
王旭 <1761680630@qq.com>
2017-11-03 12:01:48 +0800

    第一次提交

---
+++ Pods/libksygpulive/prebuilt/include/KSYStreamer/libksygpufilter.h
@@ -0,0 +1,25 @@
+//
+//  libksygpufilter.h
+//  libksygpufilter
+//
+//  Copyright (c) 2016 ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GPUImage/GPUImage.h>
+
+#import "KSYGPUFilter.h"
+#import "KSYGPUBeautifyFilter.h"
+#import "KSYGPUBeautifyExtFilter.h"
+#import "KSYGPUBeautifyPlusFilter.h"
+#import "KSYGPUDnoiseFilter.h"
+#import "KSYBeautifyFaceFilter.h"
+#import "KSYBeautifyProFilter.h"
+#import "KSYSpecialEffects.h"
+#import "KSYBuildInSpecialEffects.h"
+#import "KSYMvEffect.h"
+#import "KSYMvFilter.h"
+#import "KSYShakeFilter.h"
+
+#define KSYGPUFILTER_VER 2.9.0
+#define KSYGPUFILTER_ID  1d449e38d24532e1cbb2d55bb786d5a4e92f4edd
---
+++ Pods/libksygpulive/prebuilt/include/KSYStreamer/libksystreamerbase.h
@@ -0,0 +1,18 @@
+//
+//  libksystreamerbase.h
+//  libksystreamerbase
+//
+//  Copyright (c) 2016 ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KSYTypeDef.h"
+#import "KSYClipWriter.h"
+#import "KSYStreamerBase.h"
+#import "KSYMovieWriter.h"
+#import "KSYAVMuxer.h"
+#import "KSYMessage.h"
+#import "KSYTranscoder.h"
+
+#define KSYSTREAMERBASE_VER 2.9.0
+#define KSYSTREAMERBASE_ID  f8ba28f22fdb0fc7836e745ffabc8c30f1f74524
